dd2448fdeddbbcbffa41588a481b94342c140e05
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2025-02-19 10:01:46 -0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,42 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Usuário", type: "text" },
-        password: { label: "Senha", type: "password" },
-      },
-      async authorize(credentials) {
-        try {
-          if (!credentials || !credentials.username || !credentials.password) {
-            console.log("Credenciais inválidas");
-            return null;
-          }
+// export const authOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         username: { label: "Usuário", type: "text" },
+//         password: { label: "Senha", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         try {
+//           if (!credentials || !credentials.username || !credentials.password) {
+//             console.log("Credenciais inválidas");
+//             return null;
+//           }
 
-          console.log("Credenciais recebidas:", credentials);
+//           console.log("Credenciais recebidas:", credentials);
 
-          if (credentials.username === "admin" && credentials.password === "123456") {
-            return { id: "1", name: "Admin" };
-          }
+//           if (credentials.username === "admin" && credentials.password === "123456") {
+//             return { id: "1", name: "Admin" };
+//           }
 
-          return null;
-        } catch (error) {
-          if (error instanceof Error) {
-            console.error("Erro ao autenticar:", error.stack);
-          } else {
-            console.error("Erro desconhecido:", error);
-          }
-          return null;
-        }
-      },
-    }),
-  ],
-};
+//           return null;
+//         } catch (error) {
+//           if (error instanceof Error) {
+//             console.error("Erro ao autenticar:", error.stack);
+//           } else {
+//             console.error("Erro desconhecido:", error);
+//           }
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
+// };
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-// Next.js 13 com App Directory requer que você exporte os métodos GET e POST
-export { handler as GET, handler as POST };
+// // Next.js 13 com App Directory requer que você exporte os métodos GET e POST
+// export { handler as GET, handler as POST };
