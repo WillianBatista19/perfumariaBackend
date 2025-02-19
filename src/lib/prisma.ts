4b33fcf2fc14de19c3a5495e2cbaa834f5630d6c
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2025-02-19 11:07:23 -0300

    fix: bugs vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
-// src/lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-// Criação da instância do Prisma Client
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// Exportando a instância para ser utilizada nas rotas
-export default prisma;
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
